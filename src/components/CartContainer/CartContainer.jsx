5f4b02c53d0c908bdf3410c5223e3ae937d311a6
AlanGomez2022 <alanagustingomez@gmail.com>
2023-05-24 23:59:07 -0300

    9no commit
    incio de orden de compra

--- src/components/CartContainer/CartContainer.jsx
+++ src/components/CartContainer/CartContainer.jsx
@@ -9,34 +9,63 @@ const CartContainer = () => {
     quitProduct(product.id)
   }
 
+  const handleSubmit = () =>{
+    const orden = {
+      buyer:{name: 'Alan', phone:'1169581005', email:'alanagustingomez@gmail.com'},
+      items:[cartList.map(product =>{
+        return(
+          { id:product.id,
+            item:product.name,
+            price:product.price  
+          }
+        )
+      } )],
+      total:precioTotal()
+    }
+    console.log('enviando orden', orden)
+  }
+
   return (
     cartList.length === 0 ?
     <div className='card container-fluid'>
       <h3>No hay productos</h3>
       <Link to='/'> 
-        <button className='btn'>Ir a ver productos</button>
+        <button className='btn btn-primary text-white'>Ir a ver productos</button>
       </Link>
     </div>
     :
     <div>
         {cartList.map(product =>{
             return(
+              <>
+              <br />
               <div className='container card' key={product.id}>
-                <h2>{product.name}</h2>
-                <img src={product.foto} alt="" />
-                <h3>{product.price}</h3>
-                <p>Categoria: {product.categoria}</p>
-                <h3>Cantidad: {product.quantity}</h3>
+                
+                <div className='card-body'>
+                  <h2 className=''>{product.name}</h2>
+                  <img src={product.foto} alt="" />
+                  <h3 className='card-text'>${product.price}</h3>
+                  <p>Categoria: {product.categoria}</p>
+                  <h3>Cantidad: {product.quantity}</h3>
+                </div>
                 <button className='btn' onClick={()=>quitarProducto(product)}> Quitar del carrito 🗑</button>
+                
               </div>
-
+              <br />
+              </>
               )
         })}
         <div className="container text-center p-200">
-          <h3>Precio total: {precioTotal()}</h3>
+          <br />
+          <br />
+          <h3 className='btn-info'>Precio total: $ {precioTotal()}</h3>
+          <br /><br />
         </div>
         <div className='card container'>
-          <button className='btn container' onClick={vaciarCart}>Vaciar Carrito</button>
+          <br />
+          <button className='btn container btn-danger text-white' onClick={vaciarCart}>⛔ Vaciar Carrito ⛔</button>
+          <br />
+          <button className='btn container btn-success text-white' onClick={handleSubmit}>✅ Generar Orden ✅</button>
         </div>
     </div>
   )
